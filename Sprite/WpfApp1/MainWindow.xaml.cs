6423533fd81d823e5759e7377d1a7a943eabb6d7
aa13058219642 <aa13058219642@sohu.com>
2018-01-17 22:23:00 +0800

    update .gitignore

--- Sprite/WpfApp1/MainWindow.xaml.cs
+++ Sprite/WpfApp1/MainWindow.xaml.cs
@@ -7,13 +7,11 @@ using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 using System.Drawing;
 
-namespace WpfTest
+namespace WpfApp1
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Bitmap srcBitmap = null;
         private System.Windows.Controls.Image img = null;
         private NotifyIcon notifyIcon = null;
 
@@ -21,10 +19,18 @@ namespace WpfTest
         public MainWindow()
         {
             InitializeComponent();
+            this.AllowsTransparency = true;
+            this.WindowStyle = WindowStyle.None;
+            this.ShowInTaskbar = false;
+            this.Background = System.Windows.Media.Brushes.Transparent;
 
             //init image
+            srcBitmap = WpfApp1.Properties.Resources.defaultShell;
             img = new System.Windows.Controls.Image();
             img.Margin = new Thickness(0, 0, 0, 0);
+            img.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            img.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            this.SetBitmap(srcBitmap);
             grid1.Children.Add(img);
 
             //init NotifyIcon
@@ -37,37 +43,55 @@ namespace WpfTest
             cs.Items.Add(item);
             notifyIcon.ContextMenuStrip = cs;
             notifyIcon.Visible = true;
+
         }
 
         /// <summary>
-        /// 设置窗体图片
+        /// set image
         /// </summary>
         /// <param name="bitmap">System.Drawing.Bitmap</param>
         public void SetBitmap(Bitmap bitmap)
         {
-            BitmapImage bmp = WpfApp1.API.BitmapToBitmapImage(bitmap);
+            if (bitmap == null)
+                return;
+
+            srcBitmap = bitmap;
+            BitmapImage bmp = API.BitmapToBitmapImage(bitmap);
             this.img.Source = bmp;
             this.Width = img.Width = bmp.PixelWidth;
             this.Height = img.Height = bmp.Height;
         }
 
+        /// <summary>
+        /// set NotifyIcon's icon
+        /// </summary>
+        /// <param name="icon">System.Drawing.Icon</param>
         public void setNotifyIcon(Icon icon)
         {
             this.notifyIcon.Icon = icon;
         }
 
+        /// <summary>
+        /// Set notify menu
+        /// </summary>
+        /// <param name="menu">System.Windows.Forms.ContextMenuStrip</param>
         public void setNotifyMenu(ContextMenuStrip menu)
         {
             notifyIcon.ContextMenuStrip = menu;
         }
 
+        /// <summary>
+        /// get Ref NotifyIcon object
+        /// <para>if you want add event(like DoubleClick event) to the NotifyIcon</para>
+        /// </summary>
+        /// <returns></returns>
         public NotifyIcon getNotifyIcon()
         {
             return this.notifyIcon;
         }
 
 
-        private void Window_Closing(object sender, CancelEventArgs e)
+        protected override void OnClosing(CancelEventArgs e)
         {
             notifyIcon.Dispose();
         }
